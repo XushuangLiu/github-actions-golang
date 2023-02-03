65fd48ca70fe71a43e0b679d6ed337398c227514
XushuangLiu <120515599+XushuangLiu@users.noreply.github.com>
2023-02-03 15:45:12 -0800

    Update actions.go

--- actions.go
+++ actions.go
@@ -14,6 +14,7 @@ func Demo() {
 	fmt.Printf("Go version: %s\n", runtime.Version())
 	fmt.Printf("GOOS: %s\n", runtime.GOOS)
 	fmt.Printf("GOARCH: %s\n", runtime.GOARCH)
+	fmt.Printf("hello")
 
 	fmt.Println(quote.Go())
 }
